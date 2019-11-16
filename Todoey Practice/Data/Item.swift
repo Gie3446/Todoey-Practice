5a7f8b700a275c7354c86180e91997f60d957940
Gie3446 <virgielyn.jordan@yahoo.com>
2019-11-16 00:26:39 -0500

    Persist data using User Defaults

---
+++ Todoey Practice/Data/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Data Model.swift
+//  Todoey Practice
+//
+//  Created by VIRGIE LYN JORDAN on 11/15/19.
+//  Copyright © 2019 VIRGIE LYN JORDAN. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
